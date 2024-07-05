55317d6ca25b70e33898e5f987120743501afba8
mercurialli <ninoklala2@gmail.com>
2024-07-05 15:02:21 +0300

    refactoring pt 1

---
+++ components/game-new/ui/game-layout.jsx
@@ -0,0 +1,19 @@
+export function GameLayout({ backLink, title, gameInfo, playersList }) {
+  return (
+    <div className="pl-2">
+      <div className="pl-2">
+        {backLink}
+        {title}
+        {gameInfo}
+      </div>
+
+      <div
+        className={
+          "mt-4 bg-white rounded-2xl shadow-md px-8 py-4 justify-between grid grid-cols-2 gap-3"
+        }
+      >
+        {playersList}
+      </div>
+    </div>
+  );
+}
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,75 +1,19 @@
-import { useState } from "react";
-import {
-  GameInfo,
-  GameTitle,
-  GameField,
-  useGameState,
-  GameSymbol,
-} from "../components/game";
+import { Game } from "../components/game-new";
 import { Header } from "../components/header";
-import { UiModal } from "../components/uikit/ui-modal";
-import { UiButton } from "../components/uikit/ui-button";
 
 export default function HomePage() {
-  const [playersCount] = useState(3);
-  const {
-    cells,
-    currentMove,
-    handleCellClick,
-    nextMove,
-    winnerSequence,
-    handlePlayerTimeOver,
-    winnerSymbol,
-  } = useGameState(playersCount);
+  return (
+    <HomePageLayout header={<Header />}>
+      <Game />
+    </HomePageLayout>
+  );
+}
 
+function HomePageLayout({ header, children }) {
   return (
     <div className="bg-slate-50 min-h-screen">
-      <Header />
-      <main className="pt-6 pb-6 mx-auto w-max">
-        <GameTitle playersCount={playersCount} />
-        <GameInfo
-          playersCount={playersCount}
-          className="mt-4 "
-          currentMove={currentMove}
-          isWinner={!!winnerSymbol}
-          onPlayerTimeOver={handlePlayerTimeOver}
-        />
-        {winnerSymbol && (
-          <div className="w-4">
-            <GameSymbol symbol={winnerSymbol} />
-          </div>
-        )}
-        <UiModal
-          width="md"
-          isOpen={winnerSymbol}
-          onClose={() => console.log("Close")}
-        >
-          <UiModal.Header>Игра завершена!</UiModal.Header>
-          <UiModal.Body>
-            <div className="text-sm">
-              Победитель: <span className="text-teal-600">Paromovevg</span>
-            </div>
-          </UiModal.Body>
-          <UiModal.Footer>
-            <UiButton size="md" variant="outline">
-              Вернуться
-            </UiButton>
-            <UiButton size="md" variant="primary">
-              Играть снова
-            </UiButton>
-          </UiModal.Footer>
-        </UiModal>
-        <GameField
-          playersCount={playersCount}
-          className="mt-6 "
-          cells={cells}
-          currentMove={currentMove}
-          nextMove={nextMove}
-          handleCellClick={handleCellClick}
-          winnerSequence={winnerSequence}
-          winnerSymbol={winnerSymbol}
-        />
-      </main>
+      {header}
+      <main className="pt-6 mx-auto w-max">{children}</main>
     </div>
   );
 }
